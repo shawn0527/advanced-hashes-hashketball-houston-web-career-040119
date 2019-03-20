b1193a68713dde4073d0bb6abd0bfbda09d59672
Shawn Ren <doggy-ren@hotmail.com>
2019-03-20 19:32:30 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -279,7 +279,7 @@ def player_with_longest_name
           if
             longest_name == ""
             longest_name = player_name
-          elsif
+          else
             longest_name < player_name
             longest_name = player_name
           end

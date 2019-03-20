61d5196f66ffc7d65d7741ce9ca9cb9da9b1c71d
Shawn Ren <doggy-ren@hotmail.com>
2019-03-20 18:25:10 +0000

    Automatically backed up by Learn

--- spec/hashketball_spec.rb
+++ spec/hashketball_spec.rb
@@ -161,7 +161,7 @@ describe 'hashketball' do
         :number => 0,
         :shoe => 16,
         :points => 22,
-        :rebounds => 12,
+        :rebounds => 11,
         :assists => 12,
         :steals => 3,
         :blocks => 1,

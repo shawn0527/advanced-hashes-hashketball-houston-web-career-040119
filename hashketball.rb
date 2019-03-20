e49dadfaa8dcac9cb64440b9e9e4cb5aed028695
Shawn Ren <doggy-ren@hotmail.com>
2019-03-20 18:25:01 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -8,7 +8,7 @@ def game_hash
           :number => 0,
           :shoe => 16,
           :points => 22,
-          :rebounds => 12,
+          :rebounds => 11,
           :assists => 12,
           :steals => 3,
           :blocks => 1,
